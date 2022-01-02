97326af5e4c09b0ea521a53f61ae9c527daf5949
doubledi2000 <doanmanhdong2000@gmail.com>
2022-01-02 14:35:23 +0700

    deploy

---
+++ src/main/java/com/huce/LTUDM/IT2/OnlineTest/OnlineTest/config/ApiSecurityConfig.java
@@ -0,0 +1,10 @@
+//package com.huce.LTUDM.IT2.OnlineTest.OnlineTest.config;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@Configuration
+//public class ApiSecurityConfig  extends WebSecurityConfigurerAdapter {
+//
+//}
